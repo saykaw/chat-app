44e11889b6428aedb2505fd8b7614743dd441cff
Sayali <saykaw16@gmail.com>
2023-11-13 17:42:33 +0530

    added models

---
+++ backend/models/userModel.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+const userModel = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    picture :{
+        type: String,
+        default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
+    }
+}, {
+    timestamps: true,
+})
+
+const User = mongoose.model("User", userModel)
+
+export default User;
