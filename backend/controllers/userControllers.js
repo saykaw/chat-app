4425fb7f237eafd952ff5cd612a890846bfcd93b
Sayali <saykaw16@gmail.com>
2023-11-17 19:16:15 +0530

    authentication + apis : done

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,71 @@
+import { generateToken } from "../config/generateToken.js"
+import User from "../models/userModel.js"
+import asyncHandler from 'express-async-handler'
+
+export const registerUser = asyncHandler(async(req,res) => {
+    const {name,email,password,picture} = req.body
+
+    if (!name  || !email || !password) {
+        res.status(400)
+        throw new Error("Please Enter All Fields !!")
+    }
+
+    const userExists = await User.findOne({email})
+
+    if(userExists){
+        res.status(400)
+        throw new Error("User Already Exists !!") 
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        picture 
+    })
+
+    if (user){
+        res.status(201).json({
+            _id: user._id,
+            name:user.name,
+            email:user.email,
+            picture:user.picture,
+            token:generateToken(user._id)
+        })
+    }else{
+        res.status(400)
+        throw new Error("Failed to Create New User !!")  
+    }
+})
+
+
+
+export const authUser = asyncHandler(async(req,res)=>{
+    const {email,password} = req.body
+
+    const user = await User.findOne({email})
+
+    if(user && (await user.matchPassword(password))){
+        res.json({
+            _id: user._id,
+            name:user.name,
+            email:user.email,
+            picture:user.picture,
+            token:generateToken(user._id)
+        })
+    }
+})
+
+//'/api/user?search=sayali
+export const allUsers = asyncHandler(async(req,res)=>{
+    const keyword = req.query.search ? {
+        $or: [
+            {name : {$regex : req.query.search , $options: "i"}},
+            {email : {$regex : req.query.search , $options: "i"}}
+        ],
+    }:
+    {};
+    
+    const users = await User.find(keyword).find({_id:{$ne:req.user._id}})
+    res.send(users)
+})
