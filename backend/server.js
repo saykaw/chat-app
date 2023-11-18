693a906ef6f2c4633b6cc5d3f9ba56e387af329f
Sayali <saykaw16@gmail.com>
2023-11-18 17:53:19 +0530

    modified server

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,28 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { connectDb } from './config/Db.js'
+import userRoutes from './routes/userRoutes.js'
+import chatRoutes from './routes/chatRoutes.js'
+import { errorHandler, notFound } from './middlewares/errorMiddleware.js'
 
-const app = express()
+
 dotenv.config()
+connectDb()
+const app = express()
+app.use(express.json())
 
 const PORT = process.env.PORT
 
 app.get('/',(req,res)=>{
     res.send('Hello Express')
 })
+
+app.use('/api/user',userRoutes)
+app.use('/api/chat',chatRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
 
 app.get('/')
 
